a933e6ad84561fd22fd152733cd37a54214a8025
Vignesh Duraisamy <vigneshd332@gmail.com>
2022-01-16 17:20:57 +0530

    feat(question): Route, Model
    * Auth / JWT handler is taken to controllers

--- server/main.py
+++ server/main.py
@@ -5,13 +5,16 @@ Main File
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from server.config.settings import settings
-from server.routers import auth
-
+from server.routers import auth, questions
+from server.config.database import engine, Base
 
 app = FastAPI()
 app.include_router(auth.router)
+app.include_router(questions.router)
 
 origins = [settings.frontend_url]
+
+Base.metadata.create_all(bind=engine)
 
 
 app.add_middleware(
--- server/schemas/__init__.py
+++ server/schemas/__init__.py
@@ -3,5 +3,6 @@ Schemas for the API.
 """
 
 from server.schemas.users import Users
+from server.schemas.questions import Questions
 
-models = [Users]
+models = [Users, Questions]
