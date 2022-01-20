a7e0304f938d691acc43c270c7610cb44258d6a8
KarthikSundar2002 <karthiksundar30092002@gmail.com>
2022-01-20 16:31:55 +0530

    fix: Added Preferences
    Allows 6 Preferences

--- server/routers/preferences.py
+++ server/routers/preferences.py
@@ -60,45 +60,21 @@ async def post_preferences(
             database.close()
             raise GenericError("User Not found")
 
-        if (
-            database.query(Preferences)
-            .filter_by(user_id=user.id, preference_no=1)
-            .count()
-        ):
-            database.query(Preferences).filter_by(
-                user_id=user.id, preference_no=1
-            ).delete()
-
-        if (
-            database.query(Preferences)
-            .filter_by(user_id=user.id, preference_no=2)
-            .count()
-        ):
-            database.query(Preferences).filter_by(
-                user_id=user.id, preference_no=2
-            ).delete()
-
-        if (
-            database.query(Preferences)
-            .filter_by(user_id=user.id, preference_no=3)
-            .count()
-        ):
-            database.query(Preferences).filter_by(
-                user_id=user.id, preference_no=3
-            ).delete()
-
-        new_preference_1 = Preferences(
-            user.id, 1, preferences.preference_1
-        )
-        new_preference_2 = Preferences(
-            user.id, 2, preferences.preference_2
-        )
-        new_preference_3 = Preferences(
-            user.id, 3, preferences.preference_3
-        )
-        database.add(new_preference_1)
-        database.add(new_preference_2)
-        database.add(new_preference_3)
+        for i in range(1, 7):
+            if (
+                database.query(Preferences)
+                .filter_by(user_id=user.id, preference_no=i)
+                .count()
+            ):
+                database.query(Preferences).filter_by(
+                    user_id=user.id, preference_no=i
+                ).delete()
+        database.add(Preferences(user.id, 1, preferences.preference_1))
+        database.add(Preferences(user.id, 2, preferences.preference_2))
+        database.add(Preferences(user.id, 3, preferences.preference_3))
+        database.add(Preferences(user.id, 4, preferences.preference_4))
+        database.add(Preferences(user.id, 5, preferences.preference_5))
+        database.add(Preferences(user.id, 6, preferences.preference_6))
         database.commit()
         database.close()
         logger.info(f"{email} form preferences submitted")
