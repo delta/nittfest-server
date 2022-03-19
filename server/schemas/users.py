ebda01da6f63dc4ee40dd370a9bf39f094e57323
sarvesh-03 <sarveshjan18@gmail.com>
2022-03-20 00:56:16 +0530

    fix(migrations):alter column in events and users table

--- server/schemas/users.py
+++ server/schemas/users.py
@@ -17,19 +17,23 @@ class Users(Base):
     mobile_number = Column(String(255))
     gender = Column(String(255))
     department_id = Column(Integer, ForeignKey("departments.id"))
+    fcm_token = Column(String(255))
 
-    def __init__(self, name, email, mobile_number, gender, department_id):
+    def __init__(
+        self, name, email, mobile_number, gender, department_id, fcm_token
+    ):
         self.name = name
         self.email = email
         self.mobile_number = mobile_number
         self.gender = gender
         self.department_id = department_id
+        self.fcm_token = fcm_token
 
     def __repr__(self):
         """Representation of the object"""
         return f"""<name: '{self.name}', email: '{self.email}',
         mobile: '{self.mobile_number}', gender: '{self.gender}',
-        department_id: '{self.department_id}' >"""
+        department_id: '{self.department_id}',fcm_token: '{self.fcm_token}' >"""
 
     def serialize(self):
         """Return object data in easily serializeable format"""
@@ -40,4 +44,5 @@ class Users(Base):
             "mobile": self.mobile_number,
             "gender": self.gender,
             "department_id": self.department_id,
+            "fcm_token": self.fcm_token,
         }
